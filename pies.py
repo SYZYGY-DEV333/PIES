d68924328e14c46f0121335b4fdfb09b53e017df
SYZYGY-DEV333 <pipeorgan101@gmail.com>
2016-03-03 21:11:37 +0000

    Update pies.py

--- pies.py
+++ pies.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# PIES v1.1
+# PIES v2.0
 # SYZYGY-DEV333
 # Simple TCP port scanner in Python
 # Apache Version 2
@@ -7,11 +7,17 @@
 from socket import *
 import sys, time
 from datetime import datetime
+import argparse
 
-host = ''
-max_port = 5000
-min_port = 1
+parser = argparse.ArgumentParser()
+parser.add_argument("min", help="Minimum port number to scan")
+parser.add_argument("max", help="Maximum port number to scan")
+parser.add_argument("host", help="host address to scan")
+args = parser.parse_args()
 
+host = args.host
+max_port = args.max
+min_port = args.min
 def scan_host(host, port, r_code = 1):
     try:
         s = socket(AF_INET, SOCK_STREAM)
@@ -23,19 +29,11 @@ def scan_host(host, port, r_code = 1):
         pass
     return r_code
 
-try:
-    host = raw_input(">> Enter Target Host Address: ")
-except KeyboardInterrupt:
-    print("\n\n>> User Requested An Interrupt.")
-    print(">> Application Shutting Down.")
-    sys.exit(1)
-
 hostip = gethostbyname(host)
 print("\n>> Host: %s IP: %s" % (host, hostip))
 print(">> Scanning Started At %s...\n" % (time.strftime("%H:%M:%S")))
 start_time = datetime.now()
-
-for port in range(min_port, max_port):
+for port in range(eval(min_port), eval(max_port)):
     try:
         response = scan_host(host, port)
         if response == 0:
@@ -48,3 +46,4 @@ total_time_duration = stop_time - start_time
 print("\n>> Scanning Finsihed At %s ..." % (time.strftime("%H:%M:%S")))
 print(">> Scanning Duration: %s ..." % (total_time_duration))
 print(">> Have a nice day!")
+#!EoF
