a64e733a322d39273ffb0dce5feab1bdd94d62a1
SYZYGY-DEV333 <pipeorgan101@gmail.com>
2016-03-12 15:50:55 +0000

    Update pies.py
    Make compatable with python3

--- pies.py
+++ pies.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python
-# PIES v2.0
+#!/usr/bin/env python3
+# PIES v2.1
 # SYZYGY-DEV333
 # Simple TCP port scanner in Python
 # Apache Version 2
@@ -25,25 +25,25 @@ def scan_host(host, port, r_code = 1):
         if code == 0:
             r_code = code
         s.close()
-    except Exception, e:
+    except Exception as e:
         pass
     return r_code
 
 hostip = gethostbyname(host)
-print("\n>> Host: %s IP: %s" % (host, hostip))
-print(">> Scanning Started At %s...\n" % (time.strftime("%H:%M:%S")))
+print(("\n>> Host: %s IP: %s" % (host, hostip)))
+print((">> Scanning Started At %s...\n" % (time.strftime("%H:%M:%S"))))
 start_time = datetime.now()
 for port in range(eval(min_port), eval(max_port)):
     try:
         response = scan_host(host, port)
         if response == 0:
-            print(">> Port %d: Open" % (port))
-    except Exception, e:
+            print((">> Port %d: Open" % (port)))
+    except Exception as e:
         pass
 
 stop_time = datetime.now()
 total_time_duration = stop_time - start_time
-print("\n>> Scanning Finsihed At %s ..." % (time.strftime("%H:%M:%S")))
-print(">> Scanning Duration: %s ..." % (total_time_duration))
+print(("\n>> Scanning Finsihed At %s ..." % (time.strftime("%H:%M:%S"))))
+print((">> Scanning Duration: %s ..." % (total_time_duration)))
 print(">> Have a nice day!")
 #!EoF
